91ad4a61d174b07e299753e94268c38c6c96bce1
Vishal Yadav <ydvishal89@gmail.com>
2026-03-10 18:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const makeChange = (c) => {
-  // your name here
+
+  c = Number(c); // convert input to number
+
+  const q = Math.floor(c / 25);
+  c = c % 25;
+
+  const d = Math.floor(c / 10);
+  c = c % 10;
+
+  const n = Math.floor(c / 5);
+  c = c % 5;
+
+  const p = c;
+
+  return {
+    q: q,
+    d: d,
+    n: n,
+    p: p
+  };
 };
 
-// Do not the change the code below
+Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
